08815777e6b5996520a3787ea1d6df5b12651b11
SgtGroove <m.spano.cuomo@gmail.com>
2017-03-06 16:27:41 +0100

    Inserito il service per memorizzare i cookie

--- src/app/service/auth/auth.service.ts
+++ src/app/service/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, Response } from '@angular/http';
+import { Http, Headers, Response, RequestOptions, ResponseContentType } from '@angular/http';
 import { Observable } from 'rxjs';
 import 'rxjs/add/operator/map'
  
@@ -14,24 +14,25 @@ export class AuthenticationService {
     }
  
     login(username: string, password: string): Observable<boolean> {
-        return this.http.post('http://www.consigliamiundisco.it/rest/login', JSON.stringify({ username: username, password: password }))
-            .map((response: Response) => {
-                // login successful if there's a jwt token in the response
-                let token = response.json() && response.json().token;
-                if (token) {
-                    // set token property
-                    this.token = token;
- 
-                    // store username and jwt token in local storage to keep user logged in between page refreshes
-                    localStorage.setItem('currentUser', JSON.stringify({ username: username, token: token }));
- 
-                    // return true to indicate successful login
-                    return true;
-                } else {
-                    // return false to indicate failed login
-                    return false;
-                }
-            });
+        let headers      = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
+        let options       = new RequestOptions({ headers: headers, method: "POST"}); 
+        return this.http.post('http://www.consigliamiundisco.it/rest/login', JSON.stringify({ username: username, password: password }), options)
+                        .map((response: Response) => {
+                            if(response.status == 200){
+                                // login successful if there's a jwt token in the response
+                                let body =  JSON.parse(response.text());
+                                let token = body[0].token;
+                                if (token) {
+                                    this.token = token;
+                                    localStorage.setItem('currentUser', JSON.stringify({ username: username, token: token }));
+                                    return true;
+                                } else {
+                                    // return false to indicate failed login
+                                    return false;
+                                }
+                            }
+                            return false; 
+                        });
     }
  
     logout(): void {
