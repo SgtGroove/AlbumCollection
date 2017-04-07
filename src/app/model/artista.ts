18427a361e72608968ae7d1f01494bac3b3f9eb6
SgtGroove <m.spano.cuomo@gmail.com>
2017-04-07 16:37:39 +0200

    Nuova versione di nod, di npm e angular cli (1.0.0 beta)

--- src/app/model/artista.ts
+++ src/app/model/artista.ts
@@ -1,13 +1,26 @@
 export class Artista {
-
+    public id: number; 
+    public name: string; 
+    public href: string;
+    public uri: string;
+    public images: Array<Object>;
+    public popularity: string;
+    public popularityInt: number;
     constructor(
-        public id: number, 
-        public name: string, 
-        public href: string,
-        public uri: string,
-        public images: Array<Object>[3],
-        public popularity: string,
-        public popularityInt: number
-    ){}
-
+         id: number, 
+         name: string, 
+         href: string,
+         uri: string,
+         images: Array<Object>,
+         popularity: string,
+         popularityInt: number
+    ){
+        this.id    = id;
+        this.name  = name;
+        this.href  = href;
+        this.uri   = uri;
+        this.images=  images;
+        this.popularity     = popularity;
+        this.popularityInt  = popularityInt;
+    }
 }
